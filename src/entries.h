87e8abf0451457e3ae0d0279b83ebf77ef54f01d
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2024-02-03 17:57:58 -0500

    Add hkey methods

--- src/entries.h
+++ src/entries.h
@@ -6,7 +6,7 @@
 /*
  *                 hkey
  * --------------------------------------
- * bucket ID - Lslot ID - Fingerprint ID
+ * bucket ID - Lslot ID - Fingerprint
  * --------------------------------------
  * N-1 N-2 N-3 ... ... ... ... ... 2 1 0
  *
@@ -14,10 +14,11 @@
 
 typedef uint64_t ukey[4]; // Assume ukey to be 32 bytes
 typedef uint64_t payload[4]; // Assume payload to be 32 bytes
-typedef uint64_t hkey[2];
 
 struct hkey {
   uint64_t hkey[2];
 
-
+  uint32_t get_bucket_id();
+  uint32_t get_lslot_id();
+  uint32_t get_fingerprint();
 };
